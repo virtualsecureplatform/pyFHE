edb1669423259f00dd88f86bc93ac83f7d9eda5e
nindanaoto <dirty.knife603@gmail.com>
2019-10-26 13:26:51 +0900

    - is evaluated before <<

--- pyFHE/keyswitch.py
+++ pyFHE/keyswitch.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def IdentityKeySwitch(tlwe,ck:CloudKey):
     aibar = np.uint32(np.round(tlwe[:-1] * 2**(ck.params.basebit*ck.params.t -32)))
-    mask = 1<<ck.params.basebit - 1
+    mask = (1<<ck.params.basebit) - 1
     return np.append(np.zeros(ck.params.n),tlwe[-1]) - np.sum([[ck.ksk[i][j][aibar>>(ck.params.basebit*(ck.params.t-(j+1)))&mask] for j in range(ck.params.t)] for i in range(ck.params.n)])
