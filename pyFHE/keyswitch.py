773a4886219196a42e30f012a4ee7c5b300c2f8b
Ushitora Anqou <ushitora@anqou.net>
2019-11-11 15:26:10 +0900

    Make IdentityKeySwitch faster

--- pyFHE/keyswitch.py
+++ pyFHE/keyswitch.py
@@ -3,21 +3,24 @@ from .key import lweParams
 import numpy as np
 
 
+def calc_rhs(ck, aibar, mask):
+    indices_rhs = ck.params.basebit * np.arange(ck.params.t)[::-1]
+    indices_lhs = np.tile(aibar, (ck.params.t, 1)).T
+    indices = indices_lhs >> indices_rhs & mask
+
+    first = np.repeat(np.arange(ck.params.N), ck.params.t)
+    second = np.tile(np.arange(ck.params.t), ck.params.N)
+
+    t = ck.ksk[first, second, indices.ravel()]
+    t = t.reshape((ck.params.N, ck.params.t, -1))
+    return t.sum(axis=1).sum(axis=0)
+
+
 def IdentityKeySwitch(tlwe, ck: CloudKey):
     aibar = np.uint32(np.round(tlwe[:-1] * 2 ** (ck.params.basebit * ck.params.t - 32)))
     mask = (1 << ck.params.basebit) - 1
-    return np.uint32(np.append(np.zeros(ck.params.n), tlwe[-1])) - np.sum(
-        [
-            np.sum(
-                [
-                    ck.ksk[i][j][
-                        aibar[i] >> (ck.params.basebit * (ck.params.t - (j + 1))) & mask
-                    ]
-                    for j in range(ck.params.t)
-                ],
-                axis=0,
-            )
-            for i in range(ck.params.N)
-        ],
-        axis=0,
-    )
+
+    lhs = np.uint32(np.append(np.zeros(ck.params.n), tlwe[-1]))
+    rhs = calc_rhs(ck, aibar, mask)
+
+    return lhs - rhs
