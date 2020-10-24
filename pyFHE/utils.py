03a1efe620763c48ee3e583e6bac4f994f24dc08
nindanaoto <30498364+nindanaoto@users.noreply.github.com>
2020-10-24 16:02:52 +0900

    Add comment

--- pyFHE/utils.py
+++ pyFHE/utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+# Double to Torus32
 def dtot32(d:np.float):
     return np.uint32(np.round((d%1)*(2**32)))
 
+# Modular Gaussian distribution
 def gaussian32(mu,alpha:float,size = 1):
     return dtot32(np.random.normal(0,alpha,size)) + np.uint32(mu)
